c1f8c68e647bf5a35d4ed1f4ac811d7cf9d0dc85
Rutger <rutger.smit@gmail.com>
2020-12-08 23:18:46 +0100

    👌 even more install improvements

--- install.sh
+++ install.sh
@@ -1,41 +1,47 @@
 #! /bin/bash
 
 if [ "$EUID" -ne 0 ]
-  then echo "🧙‍ Please run as root"
+  then echo "🧙‍  Please run as root"
   exit
 fi
 
 DIR="/data/disk1"
 
-echo -n "❓ Is the external drive mounted to $DIR? (y/n) "
+echo -n "❓  Is the external drive mounted to $DIR? (y/n) "
 read mounted
 
 if [ "$mounted" != "${mounted#[Yy]}" ] ;then
     #echo "Ok, let's check"
     if [ ! -d $DIR ]; then
       # Take action if $DIR exists. #
-      echo "❌ Could not find the mounted disk on $DIR, please check the mount and try again."
+      echo "❌  Could not find the mounted disk on $DIR, please check the mount and try again."
       exit
     fi
-    echo "✔ Disk mounted to the right location, let's continue!"
+    echo "✔  Disk mounted to the right location, let's continue!"
 else
     exit
 fi
 
+echo "🔍 Checking if docker is installed"
 if ! command -v docker &> /dev/null
 then
-    echo "❌ docker could not be found, installing it"
+    echo "❌  docker could not be found, installing it"
     apt update
     apt install docker.io -y
-	echo "✔ docker succesfully installed"
+	echo "✔  docker succesfully installed"
+else
+    echo "✔  docker already installed"
 fi
 
+echo "🔍  Checking if docker compose is installed"
 if ! command -v docker &> /dev/null
 then
-    echo "❌ docker compose could not be found, installing it"
+    echo "❌  docker compose could not be found, installing it"
     apt update
     apt install docker-compose -y
-	echo "✔ docker compose succesfully installed"
+	echo "✔  docker compose succesfully installed"
+else
+    echo "✔  docker compose already install"
 fi
 
 # create some dirs for persistent storage for Docker containers
@@ -44,14 +50,14 @@ mkdir -p $DIR/mariadb
 chown -R www-data /var/docker/nextcloud
 
 
-echo "🔍 Checking if Portainer is installed"
+echo "🔍  Checking if Portainer is installed"
 if [ ! "$(docker ps -q -f name=portainer)" ]; then
-  echo "❌ Portainer is not installed, installing it right now. Why? Because I'm lazy"
+  echo "❌  Portainer is not installed, installing it right now. Why? Because I'm lazy"
   mkdir -p /var/docker/portainer
   
   # install portainer because...
   docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/docker/portainer:/data portainer/portainer-ce
-  echo "✔ Portainer succesfully installed"
+  echo "✔  Portainer succesfully installed"
 fi
 
 
@@ -61,7 +67,7 @@ echo "MYSQLPASSWORD=$PASSWORD" > .env
 echo "DATAROOT=$DIR" >> .env
 
 # bring up the container stack
-echo "🚀️ Starting the docker stack"
+echo "🚀️  Starting the docker stack"
 docker-compose -p backupstack -f backupstack.yaml up --detach
 
-echo "🧘 Done, everything should be running"
+echo "🧘  Done, everything should be running"
