0cbf6c15333bf9eb573866bc591e4373f887e8b5
Rutger Smit <rutger.smit@gmail.com>
2021-01-06 00:00:52 +0100

    🧹 Added a cleanup script

---
+++ clean.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+if [ "$EUID" -ne 0 ]
+
+  then echo "🧙  please run me as root"
+  exit
+
+else
+  echo "🧹  cleaning..."
+
+  docker-compose -f backupstack.yaml down
+  rm -rf /data/docker/*
+  rm -rf /data/disk1/*
+
+  echo "🤷  done, everything is gone"
+
+fi
